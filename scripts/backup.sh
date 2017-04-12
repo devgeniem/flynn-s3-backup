552dfa26575c9ae7d72e49730ad3e9aaa35bf1cf
Miika Arponen <miika@geniem.com>
2017-04-12 14:19:40 +0300

    new features, PIPE-117

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ##
-# Somehow cron replaces some environmental things and minio client can't use the 
+# Somehow cron replaces some environmental things and minio client can't use the
 # s3 host we configured so just use sleep instead
 ##
 
@@ -12,8 +12,8 @@ if [ -z "$FLYNN_AUTH_KEY" ]; then
 	exit 1
 fi
 
-if [ -z "$BACKUP_INTERVAL" ]; then
-	echo "ERROR: BACKUP_INTERVAL must be set"
+if [ -z "$BACKUP_INTERVAL_SECONDS" ]; then
+	echo "ERROR: BACKUP_INTERVAL_SECONDS must be set"
 	sleep 5
 	exit 1
 fi
@@ -39,14 +39,102 @@ sleep $BACKUP_INTERVAL_SECONDS
 # Take full backup from flynn
 curl -s -u :$FLYNN_AUTH_KEY -o /tmp/flynn-backup.tar http://controller.discoverd/backup
 
-# Debug the backup tar
-mc ls /tmp/flynn-backup.tar --json
+# Check if the tar file is valid
+listing=$(tar -tvf /tmp/flynn-backup.tar)
+
+if [ $? -ne 0  ]; then
+	echo "[ERROR]: Tar file is not valid!"
+	curl -X POST -H 'Content-type: application/json' \
+		--data "{\"text\":\"Flynn cluster backups: downloaded tar file is not valid.\"}" \
+		$SLACK_URL
+	sleep 5
+	exit 1
+fi
+
+# Check that all necessary files are present
+files="mysql.sql.gz postgres.sql.gz mongodb.archive.gz flynn.json"
+
+for file in ${files}; do
+	echo $listing | grep "$file"
+
+	if [ $? -ne 0 ]; then
+		echo "[ERROR]: File $file is missing from the tar archive"
+		curl -X POST -H 'Content-type: application/json' \
+			--data "{\"text\":\"Flynn cluster backups: file $file is missing from the tar archive.\"}" \
+			$SLACK_URL
+		sleep 5
+		exit 1
+	fi
+done
+
+# Check that the gzip files are valid
+cd /tmp
+
+tar -xf flynn-backup.tar
+
+tar -tf /tmp/flynn-backup.tar | cut -d '"' -f 2 | while read file; do
+    echo $file | grep ".gz"
+
+	if [ "$?" -ne 1 ]; then
+		gunzip -t "/tmp/$file"
+
+		if [[ $? -ne 0 ]]; then
+			echo "[ERROR]: File $file is not a valid gzip file"
+			curl -X POST -H 'Content-type: application/json' \
+				--data "{\"text\":\"Flynn cluster backups: file $file is not a valid gzip file.\"}" \
+				$SLACK_URL
+			sleep 5
+			exit 1
+		fi
+	fi
+done
+
+# Check that the filesize is close enough to the previous
+previous=$(mc ls s3/${AWS_S3_BUCKET}/backups/flynn-backup.tar | awk '{print $4}' | sed -e 's/[^0-9]//g')
+let current=$(ls -sh /tmp/flynn-backup.tar | sed -e 's/[^0-9]//g')/1000
+let diff=$current-$previous
+abs=$(echo $diff | tr -d -)
+let percentage=$abs*100/$previous
+
+if [ $percentage -gt 20 ]; then
+	echo "[NOTICE]: The size of the new backup file differs more than 20% from the previous one"
+	curl -X POST -H 'Content-type: application/json' \
+		--data "{\"text\":\"Flynn cluster backups: The size of the new backup file differs more than 20% from the previous one.\"}" \
+		$SLACK_URL
+	sleep 5
+fi
+
+# Notify admins if backup fails
+if [ -z "$SLACK_URL" ]; then
+	echo "ERROR: SLACK_URL must be set for notifying admins about S3 backup"
+else
+	minimumsize=90000
+	actualsize=$(wc -c <"/tmp/flynn-backup.tar")
+	if [ $actualsize -ge $minimumsize ]; then
+	    echo "[INFO]: Backup succeeded from flynn"
+	else
+	    echo "[ERROR]: Backup is too small, alerting admins that is wrong..."
+	    curl -X POST -H 'Content-type: application/json' \
+			--data "{\"text\":\"Flynn cluster backups to AWS S3 bucket: $AWS_S3_BUCKET are failing.\"}" \
+	 		$SLACK_URL
+	 	sleep 5
+	 	exit 1
+	fi
+fi
 
 # Transfer the backup into s3
-mc cp /tmp/flynn-backup.tar s3/${AWS_S3_BUCKET}/backups/flynn-backup.tar --json
+transfer=$(mc cp /tmp/flynn-backup.tar s3/${AWS_S3_BUCKET}/backups/flynn-backup.tar --json)
+if [ $? -ne 0  ]; then
+	echo "[ERROR]: Transfer to S3 failed"
+	curl -X POST -H 'Content-type: application/json' \
+		--data "{\"text\":\"Flynn cluster backups: Transfer to S3 failed: $transfer\"}" \
+		$SLACK_URL
+	sleep 5
+	exit 1
+fi
 
-# Remove the backup file
-mc rm /tmp/flynn-backup.tar --json
+# Remove the temp files
+rm -rf /tmp/*
 
 # Flynn will spin up new backup process to another machine when this exits
 exit 0
