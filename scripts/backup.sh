33bf2dd95320b322b953202e4c5247305d0d59a6
Juhani Hakanen <juhani.hakanen@geniem.com>
2017-12-21 01:40:00 +0200

    Removed mysql.gz check from backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -72,7 +72,7 @@ if [ $? -ne 0  ]; then
 fi
 
 # Check that all necessary files are present
-files="mysql.sql.gz postgres.sql.gz mongodb.archive.gz flynn.json"
+files="postgres.sql.gz mongodb.archive.gz flynn.json"
 
 for file in ${files}; do
 	if [[ $ENVIRONMENT != "seamk" || $file != "mongodb.archive.gz" ]]; then
